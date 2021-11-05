decf6a6cd4f1de9b1f565196fe70f5a472ad6b39
user <btjdghks2@gmail.com>
2021-11-05 17:44:47 +0900

    python 딕셔너리와 셋

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import copy
 
 
 def print_hi(name):
@@ -39,50 +40,120 @@ def print_hi(name):
     #     for x in range(0,3):
     #         print(x)
 
-one_marx = 'Groucho',
-type(one_marx)
-type('Groucho')
+# one_marx = 'Groucho',
+# type(one_marx)
+# type('Groucho')
+#
+# marx_tuple = ('Groucho','Chico','Harpo')
+# a,b,c = marx_tuple
+#
+# marx_list = ['Groucho','Chico','Harpo']
+# tuple(marx_list)
+#
+# list(marx_tuple)
+# print(marx_tuple)
+#
+# print(marx_tuple[0])
+# print(marx_tuple[1])
+# print(marx_tuple[2])
+# print(marx_tuple[::-1])
+# marx_list.append('Zeppo')
+# print(marx_list)
+# marx_list.insert(2,'Gummo')
+# print(marx_list)
+# print(marx_list[2] * 3)
+# numbers = [1,2,3,4]
+# numbers[1:3] = ['atp','bbt']
+# print(numbers)
+# del numbers[-1]
+# print(numbers)
+# numbers.pop()
+# numbers.pop()
+# print(numbers.pop())
+#
+#
+#
+# a = [1,2,3]
+# b = a.copy()
+# c = list(a)
+# d = a[:]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# a[0] = 'side'
+# print(a,b,c,d)
 
-marx_tuple = ('Groucho','Chico','Harpo')
-a,b,c = marx_tuple
-
-marx_list = ['Groucho','Chico','Harpo']
-tuple(marx_list)
-
-list(marx_tuple)
-print(marx_tuple)
-
-print(marx_tuple[0])
-print(marx_tuple[1])
-print(marx_tuple[2])
-print(marx_tuple[::-1])
-marx_list.append('Zeppo')
-print(marx_list)
-marx_list.insert(2,'Gummo')
-print(marx_list)
-print(marx_list[2] * 3)
-numbers = [1,2,3,4]
-numbers[1:3] = ['atp','bbt']
-print(numbers)
-del numbers[-1]
-print(numbers)
-numbers.pop()
-numbers.pop()
-print(numbers.pop())
+empty_dict = {}
 
 
+empty_dict = {"day": "A period of twenty-four hours, mostly misspent",
+              "positive": "Mistaken at the top of ine's voice",
+              "misfortune": "The kind of fortune that never misses",}
 
-a = [1,2,3]
-b = a.copy()
-c = list(a)
-d = a[:]
-print(a)
-print(b)
-print(c)
-print(d)
-a[0] = 'side'
-print(a,b,c,d)
+print(empty_dict)
 
+acme_customer = {'first': 'Wile', 'middle':'E', 'last': 'Coyote'}
+print(acme_customer)
+acme_customer = dict(first="Wile", middle="E", last="Coyote")
+print(acme_customer)
+
+lol = [['a','b'],['c','d'],['e','f']]
+dict(lol)
+{'a':'b','c':'d','e':'f'}
+print(lol)
+
+some_pythons = {
+    'Graham':'Chapman',
+    'John':'Cleese',
+    'Eric':'Idle',
+    'Terry':'Gilliam',
+    'Michael':'Palin',
+    'Terry':'Jones',
+}
+print(some_pythons.get('John'))
+print(some_pythons.get('John','Not a Python'))
+
+print(list(some_pythons.keys()))
+print(len(some_pythons))
+
+first = {'a':'agony', 'b':'bliss'}
+second = {'b':'bagels','c':'candy'}
+print({**first,**second})
+third = {'d': 'donuts'}
+print({**first,**third,**second})
+others = {'Marx': 'Groucho','Howrd':'Moe'}
+some_pythons.update(others)
+print(some_pythons)
+
+del some_pythons['Marx']
+print(some_pythons)
+print(len(some_pythons))
+print(some_pythons.pop('John'))
+some_pythons.pop('first','Hugo')
+print('Graham' in some_pythons)
+signals = {'green': 'go', 'yellow':'go faster','red':'smile for hte camera'}
+save_signals = signals
+
+print(save_signals)
+original_signls = signals.copy()
+signals['blue'] = 'confuse everyone'
+print(signals)
+print(original_signls)
+signals = {'green': 'go', 'yellow':'go faster','red': ['stop','smile']}
+print(signals)
+signals['red'][1]='sweat'
+print(signals)
+signals_copy = signals.copy()
+print(signals_copy)
+
+signals_copy = copy.deepcopy(signals)
+
+print(signals)
+
+signals['red'][1]= 'sweat'
+
+print(signals_copy)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
