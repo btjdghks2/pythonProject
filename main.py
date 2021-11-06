86b0154e4f328d02b7c45a1cd5e565a96eb04f26
sunghwan <btjdghks2@gmail.com>
2021-11-07 00:15:02 +0900

    linux update

--- main.py
+++ main.py
@@ -155,6 +155,8 @@ signals['red'][1]= 'sweat'
 
 print(signals_copy)
 
+print("Linux update!")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
